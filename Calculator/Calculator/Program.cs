25f1a12025b191cb7407251456a4677ab402dfbd
Abolmasova <valleriplus@mail.ru>
2020-04-27 22:15:30 +0300

    Add Calculator

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,67 @@
+﻿using NLog;
+using System;
+
+namespace Calculator
+{
+
+    public class Program
+    {
+        private static Logger log = LogManager.GetCurrentClassLogger();
+
+        public static void Main(string[] args)
+        {
+            log.Debug("Мной найден дебаг!");
+            while (true)
+            {
+                double a, b;
+                char z;
+                try
+
+                {
+
+                    Console.WriteLine("Введите первое число ");
+                    a = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Введите второе число ");
+                    b = Convert.ToDouble(Console.ReadLine());
+
+                }
+                catch (Exception)
+                {
+                    log.Info("Мной найден дебаг!");
+                    Console.WriteLine("Ошибка!Введите число, а не символ!");
+                    Console.ReadLine();
+                    continue;
+                }
+                log.Debug("Мной найден дебаг!");
+                Console.WriteLine("Введите действие (+, -, *, /) ");
+                z = Convert.ToChar(Console.ReadLine());
+
+                switch (z)
+                {
+                    case '+':
+                        Console.WriteLine("{0}+{1}={2}", a, b, a + b);
+                        break;
+                    case '-':
+                        Console.WriteLine("{0}-{1}={2}", a, b, a - b);
+                        break;
+                    case '*':
+                        Console.WriteLine("{0}*{1}={2}", a, b, a * b);
+                        break;
+                    case '/':
+                        Console.WriteLine("{0}/{1}={2}", a, b, a / b);
+                        break;
+                    default:
+                        log.Error("Мной найден дебаг!");
+                        Console.WriteLine("Ошибка");
+                        break;
+                }
+                Console.ReadLine();
+                break;
+            }
+
+
+        }
+
+
+    }
+}
